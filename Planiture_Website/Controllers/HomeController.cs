2996b099d10bd9999c66a692162b58fcc4d6e892
kingz will <kingzwill41@gmail.com>
2020-06-02 23:02:49 -0500

    Updated the Account Customer serivce, investments, menu, planiture academy, security and subscriptions pages. I also updated the Account Controller class as well as the site.cs file

--- Planiture_Website/Controllers/HomeController.cs
+++ Planiture_Website/Controllers/HomeController.cs
@@ -28,6 +28,36 @@ namespace Planiture_Website.Controllers
             return View();
         }
 
+        public IActionResult Menu()
+        {
+            return View();
+        }
+
+        public IActionResult Investments()
+        {
+            return View();
+        }
+
+        public IActionResult Subscriptions()
+        {
+            return View();
+        }
+
+        public IActionResult PlanitureAcademy()
+        {
+            return View();
+        }
+
+        public IActionResult Security()
+        {
+            return View();
+        }
+
+        public IActionResult CustomerService()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
