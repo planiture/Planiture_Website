e3051c90e85adb8dcad86e92f0145354ab746d83
kingz will <kingzwill41@gmail.com>
2020-06-05 23:57:45 -0500

    updated the upload/sign documents page

--- Planiture_Website/wwwroot/js/site.js
+++ Planiture_Website/wwwroot/js/site.js
@@ -9,3 +9,39 @@ window.addEventListener("scroll", function () {
     header.classList.toggle("sticky", window.scrollY > 0);
 })
 
+
+//The following is for uploading files
+Array.prototype.forEach.call(document.querySelectorAll('.upload-btn'), function (button) {
+    const hiddenInput = button.parentElement.querySelector('.file-input');
+    const label = button.parentElement.querySelector('.upload-label');
+    const defaultLabelText = 'No file(s) selected';
+
+    //Sets default text for label
+    label.textContent = defaultLabelText;
+    label.title = defaultLabelText;
+
+    //this triggers the file selecting browser function
+    button.addEventListener('click', function () {
+        hiddenInput.click();
+    });
+
+    hiddenInput.addEventListener('change', function () {
+
+        const filenameList = Array.prototype.map.call(hiddenInput.files, function (file) {
+            return file.name;
+        });
+
+        label.textContent = filenameList.join(', ') || defaultLabelText;
+        label.title = label.textContent;
+    });
+})
+
+//displays deposit form on screen when the deposit form button is clicked
+document.getElementById('deposit-btn').addEventListener('click', function () {
+    document.querySelector('.modal').style.display = 'flex';
+});
+
+//close the deposit form after it has been opened
+document.querySelector('.close').addEventListener('click', function () {
+    document.querySelector('.modal').style.display = 'none';
+})
